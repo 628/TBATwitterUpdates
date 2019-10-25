9370bd427a13325e8a4834f0db3e7bea56acc6a0
Sandro Petrovski <admin@aello.dev>
2019-10-25 17:28:43 -0400

    Implement sendTweet()

--- twitterwrapper/Twitter.cpp
+++ twitterwrapper/Twitter.cpp
@@ -1,6 +1,52 @@
+#include <curl/curl.h>
+#include <ctime>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 #include "Twitter.h"
+#include "HttpRequest.h"
 
-bool Twitter::sendTweet(std::string messageContent)
+#define urlEncode(str) std::string(oauth_url_escape(str.c_str()))
+
+bool Twitter::sendTweet(const std::string& messageContent)
 {
+    OAuthHeaders oAuthHeaders;
 
+    oAuthHeaders.token = authToken;
+    oAuthHeaders.consumerKey = consumerKey;
+
+    std::time_t epochSeconds = std::time(nullptr);
+    oAuthHeaders.timestamp = std::to_string(epochSeconds);
+
+    std::string paramStr = "oauth_consumer_key=" + urlEncode(oAuthHeaders.consumerKey) +
+            "&oauth_nonce=" + urlEncode(oAuthHeaders.nonce) +
+            "&oauth_signature_method=" + urlEncode(oAuthHeaders.signature_method) +
+            "&oauth_timestamp=" + urlEncode(oAuthHeaders.timestamp) +
+            "&oauth_token=" + urlEncode(oAuthHeaders.token) +
+            "&oauth_version=" + urlEncode(oAuthHeaders.version) +
+            "&status=" + urlEncode(messageContent);
+
+    std::string url = "https://api.twitter.com/1.1/statuses/update.json";
+    std::string signatureStr = "POST&" + urlEncode(url) + "&" + urlEncode(paramStr);
+
+    std::string signingKey = urlEncode(consumerKeySecret) + "&" + urlEncode(authTokenSecret);
+
+    oAuthHeaders.signature = urlEncode(std::string(oauth_sign_hmac_sha1(signatureStr.c_str(), signingKey.c_str())));
+
+    struct curl_slist *headers = nullptr;
+    std::string headersString = "Authorization: "
+                                "OAuth "
+                                "oauth_consumer_key=\"" + oAuthHeaders.consumerKey + "\", "
+                                "oauth_nonce=\"" + oAuthHeaders.nonce + "\", "
+                                "oauth_signature=\"" + oAuthHeaders.signature + "\", "
+                                "oauth_signature_method=\"" + oAuthHeaders.signature_method + "\", "
+                                "oauth_timestamp=\"" + oAuthHeaders.timestamp + "\", "
+                                "oauth_token=\"" + oAuthHeaders.token + "\", "
+                                "oauth_version=\"" + oAuthHeaders.version + "\"";
+
+    headers = curl_slist_append(headers, headersString.c_str());
+
+    std::string contentField = "status=" + urlEncode(messageContent);
+
+    HttpRequest::sendPost(url.c_str(), headers, contentField.c_str());
 }
--- twitterwrapper/Twitter.h
+++ twitterwrapper/Twitter.h
@@ -1,31 +1,40 @@
+#include <utility>
+
+#include <utility>
+
 #ifndef TBATWITTERUPDATES_TWITTER_H
 #define TBATWITTERUPDATES_TWITTER_H
 
 #include <string>
 #include <utility>
+#include <oauth.h>
 
 class Twitter
 {
 public:
     std::string authToken;
     std::string consumerKey;
+    std::string authTokenSecret;
+    std::string consumerKeySecret;
 
-    Twitter(std::string authToken, std::string consumerKey)
+    Twitter(std::string token, std::string key, std::string tokenSecret, std::string keySecret)
     {
-        this->authToken = std::move(authToken);
-        this->consumerKey = std::move(consumerKey);
+        authToken = std::move(token);
+        consumerKey = std::move(key);
+        consumerKeySecret = std::move(keySecret);
+        authTokenSecret = std::move(tokenSecret);
     }
 
-    bool sendTweet(std::string messageContent);
+    bool sendTweet(const std::string& messageContent);
 private:
     typedef struct
     {
-        std::string consumerKey = consumerKey;
+        std::string consumerKey;
         std::string nonce = oauth_gen_nonce();
         std::string signature;
         std::string signature_method = "HMAC-SHA1";
         std::string timestamp;
-        std::string token = authToken;
+        std::string token;
         std::string version = "1.0";
     } OAuthHeaders;
 };
