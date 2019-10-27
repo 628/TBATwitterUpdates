c6c10d6bb456772bf5dcc09a5ef65c5e6e404d7a
Sandro Petrovski <admin@aello.dev>
2019-10-26 21:36:07 -0400

    Add config/listen code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <oauth.h>
+#include <nlohmann/json.hpp>
 #include "twitterwrapper/HttpRequest.h"
+#include "pistache/endpoint.h"
+#include "httpserver/handlers/WebhookHandler.h"
+#include "Config.h"
+#include "httpserver/headers/XTBAChecksum.h"
 
-int main()
+using namespace Pistache;
+
+int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " [configFilePath]" << std::endl;
+        return -1;
+    }
+
+    if (access(argv[1], F_OK) == -1)
+    {
+        std::cerr << "Config file not found!" << std::endl;
+        return -1;
+    }
+
+    Config::loadConfig(std::string(argv[1]));
+
+    Http::Header::Registry::instance().registerHeader<XTBAChecksum>();
+    Http::listenAndServe<WebhookHandler>("*:" + Config::get("port"));
 }
