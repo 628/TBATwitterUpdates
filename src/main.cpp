0b6ff7d6d6398bad188fd23340ff6133719af455
Sandro Petrovski <admin@aello.dev>
2019-10-26 23:16:41 -0400

    Make argument required

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: " << argv[0] << " [configFilePath]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <configFilePath>" << std::endl;
         return -1;
     }
 
