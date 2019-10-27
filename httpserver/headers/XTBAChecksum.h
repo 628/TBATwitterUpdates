542bbd4ba38364477788fe9ddf7283a56bd65c04
Sandro Petrovski <admin@aello.dev>
2019-10-26 21:30:59 -0400

    Add XTBAChecksum header

---
+++ httpserver/headers/XTBAChecksum.h
@@ -0,0 +1,34 @@
+#ifndef TBATWITTERUPDATES_XTBACHECKSUM_H
+#define TBATWITTERUPDATES_XTBACHECKSUM_H
+
+#include <string>
+#include <pistache/http_header.h>
+
+using namespace Pistache;
+
+class XTBAChecksum : public Http::Header::Header
+{
+public:
+    NAME("X-TBA-Checksum");
+
+    XTBAChecksum()
+            : checkSum()
+    {}
+
+    void parse(const std::string &str) override
+    {}
+
+    void write(std::ostream &os) const override
+    {}
+
+    std::string getCheckSum()
+    {
+        return checkSum;
+    }
+
+private:
+    std::string checkSum;
+};
+
+
+#endif //TBATWITTERUPDATES_XTBACHECKSUM_H
