fb51b3d447e079de0949181eb9f8628859f4a0a2
Sandro Petrovski <admin@aello.dev>
2019-10-31 09:15:29 -0400

    Add Logger

---
+++ src/Logger.cpp
@@ -0,0 +1,21 @@
+#include "Logger.h"
+
+void Logger::log(std::string log)
+{
+
+}
+
+void Logger::debug(std::string log)
+{
+
+}
+
+void Logger::error(std::string log)
+{
+
+}
+
+void Logger::fatal(std::string log)
+{
+
+}
---
+++ src/Logger.h
@@ -0,0 +1,17 @@
+#ifndef TBATWITTERUPDATES_LOGGER_H
+#define TBATWITTERUPDATES_LOGGER_H
+
+
+#include <string>
+
+class Logger
+{
+
+    static void log(std::string log);
+    static void debug(std::string log);
+    static void error(std::string log);
+    static void fatal(std::string log);
+};
+
+
+#endif //TBATWITTERUPDATES_LOGGER_H
